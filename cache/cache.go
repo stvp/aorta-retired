221fd80ba63610d5e1827f9929a9f7a2432d68e2
Tyson Tate <tyson@tysontate.com>
2014-04-04 12:27:52 -0700

    Re-write Cache with non-insane expiration.

--- cache/cache.go
+++ cache/cache.go
@@ -1,73 +1,125 @@
 package cache
 
 import (
+	"container/list"
 	"sync"
 	"time"
 )
 
-type CacheFill func() ([]byte, error)
-
-type TimeBytes struct {
-	Bytes []byte
-	Time  time.Time
-}
-
+// A Cache is a simple cache for byte slices with string keys. The primary goal
+// is to ensure that the cache fill function for a given key is never called
+// more often than needed. If you continually call Fetch() with a max age of 1
+// second ago, the cache fill function will never be called more than once a
+// second regardless of whether the cache fill function is fast or slow for a
+// given key. If a cache fill for a key is slow, Fetch() calls for that key
+// will block until the cache is filled.
+//
+// Cache is designed to hold at most millions of keys. Memory efficiency is not
+// a design goal, but the overhead for a million keys shouldn't be more than
+// 16-32 megabytes.
 type Cache struct {
-	values  map[string]TimeBytes
-	mutexes map[string]*sync.Mutex
-	sync.Mutex
+	l            list.List
+	m            map[string]*list.Element
+	mutexes      map[string]*sync.Mutex
+	mutexesMutex sync.Mutex
 }
 
+type cachedValue struct {
+	key       string
+	bytes     []byte
+	timestamp time.Time
+}
+
+// NewCache returns an initialized Cache, ready for use.
 func NewCache() *Cache {
 	return &Cache{
-		values:  map[string]TimeBytes{},
-		mutexes: map[string]*sync.Mutex{},
+		m:       make(map[string]*list.Element),
+		mutexes: make(map[string]*sync.Mutex),
 	}
 }
 
-func (c *Cache) lock(key string) {
-	c.Lock()
-	defer c.Unlock()
+// Fetch takes a key and returns the cached value, if the key is cached and is
+// not older than the given time.Time. If the key is not cached, the given
+// cache fill function will be called to fill the cache. If the cache fill
+// function returns an error, the cache will not be filled and the error will
+// be returned by Fetch.
+func (c *Cache) Fetch(key string, maxAge time.Time, fn func() ([]byte, error)) ([]byte, error) {
+	c.lockKey(key)
+	defer c.unlockKey(key)
 
+	// Try to use cached value
+	element, ok := c.m[key]
+	if ok {
+		value := element.Value.(*cachedValue)
+		if value.timestamp.After(maxAge) {
+			return value.bytes, nil
+		}
+	}
+
+	// Cache is empty or stale, fill it up
+	bytes, err := fn()
+	if err != nil {
+		return bytes, err
+	}
+
+	value := &cachedValue{
+		key:       key,
+		bytes:     bytes,
+		timestamp: time.Now(),
+	}
+	c.m[key] = c.l.PushFront(value)
+
+	return bytes, nil
+}
+
+// Expire is an exact expiration loop that expires all keys (up to a given
+// maximum count) that are older than the given time.Time. It expires the
+// oldest values first, and returns the number of values that were expired.
+// It's moderately fast: on a MacBook Pro, it expires ~2,000 items per
+// millisecond.
+func (c *Cache) Expire(maxCount int, maxAge time.Time) (expired int) {
+	var v *cachedValue
+	var cursor, prev *list.Element
+	cursor = c.l.Back()
+
+	for cursor != nil {
+		v = cursor.Value.(*cachedValue)
+		if v.timestamp.After(maxAge) {
+			break
+		}
+		prev = cursor.Prev()
+		c.remove(cursor)
+		cursor = prev
+		expired++
+		if maxCount > 0 && expired == maxCount {
+			break
+		}
+	}
+
+	return expired
+}
+
+func (c *Cache) remove(e *list.Element) {
+	value := e.Value.(*cachedValue)
+	c.lockKey(value.key)
+	c.l.Remove(e)
+	delete(c.m, value.key)
+	c.mutexesMutex.Lock()
+	delete(c.mutexes, value.key)
+	c.mutexesMutex.Unlock()
+}
+
+func (c *Cache) lockKey(key string) {
+	c.mutexesMutex.Lock()
 	mutex, ok := c.mutexes[key]
 	if !ok {
 		mutex = &sync.Mutex{}
 		c.mutexes[key] = mutex
 	}
+	c.mutexesMutex.Unlock()
 	mutex.Lock()
 }
 
-func (c *Cache) unlock(key string) {
-	mutex := c.mutexes[key]
-	mutex.Unlock()
-}
-
-func (c *Cache) Fetch(key string, maxAge time.Duration, fill CacheFill) ([]byte, error) {
-	c.lock(key)
-	defer c.unlock(key)
-
-	now := time.Now()
-
-	value, ok := c.values[key]
-	if ok && now.Sub(value.Time) < maxAge {
-		return value.Bytes, nil
-	}
-
-	b, err := fill()
-	if err != nil {
-		return b, err
-	}
-
-	c.values[key] = TimeBytes{
-		Bytes: b,
-		Time:  now,
-	}
-
-	return b, nil
-}
-
-func (c *Cache) clear(key string) {
-	c.lock(key)
-	delete(c.values, key)
-	delete(c.mutexes, key)
+func (c *Cache) unlockKey(key string) {
+	c.mutexes[key].Unlock()
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,61 +2,153 @@ package cache
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestCacheFetch(t *testing.T) {
+	now := time.Now()
+	secondAgo := now.Add(-time.Second)
+
 	cache := NewCache()
 
 	// Fetch an un-cached value
-	result, err := cache.Fetch("mykey", time.Second, func() ([]byte, error) {
+	result, err := cache.Fetch("mykey", secondAgo, func() ([]byte, error) {
 		return []byte("cool"), nil
 	})
 	if err != nil {
-		t.Error("An uncached Fetch() call returned an error when it shouldn't have")
+		t.Error(err)
 	}
 	if string(result) != "cool" {
 		t.Errorf("Fetch() returned the wrong value: %#v", result)
 	}
 
 	// Fetch a cached value
-	cache.Fetch("mykey", time.Second, func() ([]byte, error) {
+	result, err = cache.Fetch("mykey", secondAgo, func() ([]byte, error) {
 		t.Error("Fetch called the CacheFill function when the key was already cached")
 		return []byte("nope"), nil
 	})
+	if err != nil {
+		t.Error(err)
+	}
+	if string(result) != "cool" {
+		t.Errorf("Fetch() returned the wrong value: %#v", result)
+	}
 
 	// Fetch a cached but stale value
-	cache.values["mykey"] = TimeBytes{
-		Bytes: []byte("old"),
-		Time:  time.Now().Add(-time.Minute),
-	}
-	result, err = cache.Fetch("mykey", 30*time.Second, func() ([]byte, error) {
+	result, err = cache.Fetch("mykey", time.Now(), func() ([]byte, error) {
 		return []byte("even cooler"), nil
 	})
 	if err != nil {
-		t.Error("A stale, cached Fetch() call returned an error when it shouldn't have")
+		t.Error(err)
 	}
 	if string(result) != "even cooler" {
 		t.Errorf("Fetch() returned the wrong value: %#v", result)
 	}
 
 	// Fetch when the CacheFill returns an error
-	result, err = cache.Fetch("uncachedkey", time.Second, func() ([]byte, error) {
+	result, err = cache.Fetch("uncachedkey", now, func() ([]byte, error) {
 		return nil, fmt.Errorf("oh no")
 	})
 	if err == nil {
 		t.Error("Fetch() with a failed CacheFill should return an error, but it didn't")
 	}
 
-	// Fetch after a failure runs the CacheFill function
-	result, err = cache.Fetch("uncachedkey", time.Second, func() ([]byte, error) {
+	// Fetch after a failure runs the given function to fill cache
+	result, err = cache.Fetch("uncachedkey", secondAgo, func() ([]byte, error) {
 		return []byte("normal"), nil
 	})
 	if err != nil {
-		t.Error("An uncached Fetch() call returned an error when it shouldn't have")
+		t.Error(err)
 	}
 	if string(result) != "normal" {
 		t.Errorf("Fetch() returned the wrong value: %#v", result)
 	}
+}
+
+func BenchmarkFetchFillRate(b *testing.B) {
+	cache := NewCache()
+	var fills int
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	start := time.Now()
+	go func() {
+		for i := 0; i < b.N; i++ {
+			cache.Fetch("mykey", time.Now().Add(-time.Millisecond), func() ([]byte, error) {
+				fills++
+				return []byte{}, nil
+			})
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < b.N; i++ {
+			cache.Fetch("mykey", time.Now().Add(-time.Millisecond), func() ([]byte, error) {
+				fills++
+				return []byte{}, nil
+			})
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	stop := time.Now()
+	duration := stop.Sub(start)
+
+	fmt.Printf("\n%d fills in %d ms (expect 1 per ms)\t", fills, duration/time.Millisecond)
+}
+
+func TestExpire(t *testing.T) {
+	cache := NewCache()
+
+	// With no elements
+	expired := cache.Expire(-1, time.Now())
+	if expired != 0 {
+		t.Errorf("expected to expire 0 elements, got: %d", expired)
+	}
+
+	// a is oldest element
+	for _, letter := range []string{"a", "b", "c", "d", "e", "f"} {
+		cache.Fetch(letter, time.Now(), func() ([]byte, error) { return []byte{}, nil })
+	}
+	// a, b, and c should get expired
+	for _, letter := range []string{"a", "b", "c"} {
+		v := cache.m[letter].Value.(*cachedValue)
+		v.timestamp = time.Now().Add(-time.Hour)
+	}
+
+	// Expire 2 keys
+	expired = cache.Expire(2, time.Now().Add(-time.Minute))
+	if expired != 2 {
+		t.Errorf("expected to expire 2 elements, got: %d", expired)
+	}
+	if _, ok := cache.m["b"]; ok {
+		t.Error("didn't remove b, which was old")
+	}
+	if _, ok := cache.m["c"]; !ok {
+		t.Error("shouldn't remove more than 2 keys")
+	}
+
+	// Expire everything
+	expired = cache.Expire(-1, time.Now())
+	if expired != 4 {
+		t.Errorf("expected to expire 4 elements, got: %d", expired)
+	}
+	if _, ok := cache.m["c"]; ok {
+		t.Error("didn't remove c, which was old")
+	}
+}
+
+func BenchmarkExpire(b *testing.B) {
+	cache := NewCache()
+
+	for i := 0; i < b.N; i++ {
+		key := fmt.Sprintf("some_key_%d", i)
+		cache.Fetch(key, time.Now(), func() ([]byte, error) {
+			return []byte("some value here"), nil
+		})
+	}
+
+	b.ResetTimer()
+	cache.Expire(-1, time.Now())
 }
