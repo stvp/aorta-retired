4ed2ac1df47ca5ad079342697884420f89d11374
Tyson Tate <tyson@tysontate.com>
2014-04-03 17:01:54 -0700

    Add basic server config.

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
 	"github.com/stvp/aorta/proxy"
 	"time"
 )
 
+var (
+	bind      = flag.String("bind", "0.0.0.0:7979", "bind location for the TCP proxy server")
+	password  = flag.String("password", "", "required password before clients can proxy commands")
+	clientttl = flag.Int("clientttl", 300, "timeout for client connections, in seconds")
+	serverttl = flag.Int("serverttl", 2, "timeout for server connections, in seconds")
+)
+
 func main() {
-	server := proxy.NewServer("0.0.0.0:9999", "password", time.Minute, time.Second)
+	flag.Parse()
+
+	ctimeout := time.Duration(*clientttl) * time.Second
+	stimeout := time.Duration(*serverttl) * time.Second
+
+	server := proxy.NewServer(*bind, *password, ctimeout, stimeout)
 	err := server.Listen()
 	if err != nil {
 		panic(err)
